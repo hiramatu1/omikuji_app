3a71fe0592cef32298626215146424425f95230a
hiramatu1 <105049277+hiramatu1@users.noreply.github.com>
2025-10-18 23:34:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,44 +11,32 @@ omikuji_results = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶
 # おみくじを引く画面へ遷移
 @app.route('/')
 def index():
-    url = "https://www.hcs.co.jp/news/docs/test.pdf"
-    
-    with request.urlopen(url) as res:
-        f = BytesIO(res.read())
-        text = extract_text(f)
-        print(text[:300])
-    print("hello2")
-    return render_template('index.html')
+    html = '''
+    <form action="/test">
+        <p><label>test: </label>
+        <input type="text" name="query" value="default">
+        <button type="submit" formmethod="get">GET</button>
+        <button type="submit" formmethod="post">POST</button></p>
+    </form>
+    '''
+    return Markup(html)
 
-# おみくじの結果をランダムで選択し、結果画面へ遷移
-@app.route('/omikuji')
-def omikuji():
-    result = random.choice(omikuji_results)
-    return render_template('result.html', result=result)
 
-if __name__ == '__main__':
-
-    app.run(debug=True)
-
-#GET,POST TEST
 @app.route('/test', methods=['GET', 'POST'])
 def test():
     try:
         if request.method == 'GET':
-            print("get1")
             return request.args.get('query', '')
         elif request.method == 'POST':
-            print("post1")
             return request.form['query']
         else:
-            print("else1")
             return abort(400)
     except Exception as e:
-        print("e")
         return str(e)
 
 
-
+if __name__ == '__main__':
+    app.run()
 
 
 
