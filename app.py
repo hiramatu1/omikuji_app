eacf95b962f91f5b23f57d5e1407b49734b48008
hiramatu1 <105049277+hiramatu1@users.noreply.github.com>
2025-10-19 01:02:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
@@ -43,6 +43,7 @@ def sample_form():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
