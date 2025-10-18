be1a73ea7bbf86fe1d589e5a678cc5f3f6eff88e
hiramatu1 <105049277+hiramatu1@users.noreply.github.com>
2025-10-18 22:47:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import random
 from io import BytesIO
 from urllib import request
 from pdfminer.high_level import extract_text
-
-
+    
 app = Flask(__name__)
 
 omikuji_results = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶']
@@ -12,6 +11,12 @@ omikuji_results = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶
 # おみくじを引く画面へ遷移
 @app.route('/')
 def index():
+    url = "https://www.jitec.ipa.go.jp/1_00topic/topic_20071225_shinseido_4.pdf"
+    
+    with request.urlopen(url) as res:
+        f = BytesIO(res.read())
+        text = extract_text(f)
+        print(text[:300])
     print("hello2")
     return render_template('index.html')
 
@@ -24,6 +29,7 @@ def omikuji():
 if __name__ == '__main__':
 
     app.run(debug=True)
+
 
 
 
