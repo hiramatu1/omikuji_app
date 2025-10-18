a468ead229769407073b201433d48067abe4ec2f
hiramatu1 <105049277+hiramatu1@users.noreply.github.com>
2025-10-18 21:50:55 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+omikuji_results = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶']
+
+# おみくじを引く画面へ遷移
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# おみくじの結果をランダムで選択し、結果画面へ遷移
+@app.route('/omikuji')
+def omikuji():
+    result = random.choice(omikuji_results)
+    return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
