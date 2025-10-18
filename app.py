e6e83ea84a8f13b2260ace3219e88a7e12f117cd
hiramatu1 <105049277+hiramatu1@users.noreply.github.com>
2025-10-19 00:09:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
 from flask import Flask, render_template
 import random
-from io import BytesIO
-from urllib import request
-from pdfminer.high_level import extract_text
-import requests
 
+app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def Hello():
+omikuji_results = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶']
+
+# おみくじを引く画面へ遷移
+@app.route('/')
+def index():
     return render_template('index.html')
 
-@app.route('/', methods=['GET'])
-def view():
-    input_data = request.args.get('item')
-    return render_template('index.html', item = input_data)
+# おみくじの結果をランダムで選択し、結果画面へ遷移
+@app.route('/omikuji')
+def omikuji():
+    result = random.choice(omikuji_results)
+    return render_template('result.html', result=result)
 
-
-
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
