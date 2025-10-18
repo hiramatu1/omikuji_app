56bd069e158612ee8df95181cc27086c88b0621f
hiramatu1 <105049277+hiramatu1@users.noreply.github.com>
2025-10-18 22:30:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from io import BytesIO
 from urllib import request
 from pdfminer.high_level import extract_text
 
+url = "https://www.jitec.ipa.go.jp/1_00topic/topic_20071225_shinseido_4.pdf"
+
 with request.urlopen(url) as res:
     f = BytesIO(res.read())
     text = extract_text(f)
@@ -39,7 +41,6 @@ with request.urlopen(url) as res:
 
 
 
-
 app = Flask(__name__)
 
 omikuji_results = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶']
@@ -59,4 +60,5 @@ def omikuji():
 if __name__ == '__main__':
 
     app.run(debug=True)
+
 
